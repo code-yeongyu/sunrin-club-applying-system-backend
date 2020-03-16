3de056c3572ad5ffbb67ad12211383b5437ac380
Yeon Gyu Kim <public.kim.yeon.gyu@gmail.com>
2020-03-16 16:08:36 +0900

    Remove text length limit

--- backend/application/models.py
+++ backend/application/models.py
@@ -8,8 +8,8 @@ class Application(models.Model):
                             choices=[('uf', 'unifox'), ('em', 'emotion'),
                                      ('tl', 'teamlog'), ('l7', 'layer7'),
                                      ('nf', 'nefus')])
-    name = models.CharField(max_length=5, null=False)
-    number = models.CharField(max_length=5, null=False)
-    phone_number = models.CharField(max_length=11, null=False)
+    name = models.TextField(null=False, blank=False)
+    number = models.TextField(null=False, blank=False)
+    phone_number = models.TextField(null=False, blank=False)
     email = models.EmailField(null=False)
     content = models.TextField(null=False, blank=True)
