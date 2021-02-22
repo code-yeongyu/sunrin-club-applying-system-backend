8161fac7a63b5fb65daa0cb791fab6967a159ccd
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-02-22 12:22:04 +0900

    Update .gitignore to ignore myvenv/ and .vscode/

--- backend/myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/yeongyu/Documents/git/club-applying-system-backend/backend/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
